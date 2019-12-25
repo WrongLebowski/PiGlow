affe3e8da6d6c2fb8d0afdc53fc766bbaf231493
WrongLebowski <WrongLebowski@users.noreply.github.com>
2019-12-24 22:44:14 -0600

    Update and rename halloween.py to redGreen.py
    Merry x-mas

--- Examples/redGreen.py
+++ Examples/redGreen.py
@@ -1,5 +1,5 @@
 ##################################################
-## Switch only yellow, orange and red to random ##
+## Switch only green and red to random          ##
 ## brightness                                   ##
 ## Example by tng - @TommyBobbins               ##
 ##################################################
@@ -9,23 +9,25 @@ from time import sleep
 import random
 
 # Maximum random sleep between switching an LED on or off
-sleep_period = 0.001
+sleep_period = 0.1
 
 piglow = PiGlow()
 # Switch off all the lights first
 piglow.all(0)
 
-# We only want to select the Red, Orange and Yellow LEDs (roy)
-roy_leds = [ "01","02","03", "07", "08", "09", "13","14", "15" ]
+# We only want to select the Red, and Green LEDs (redGreen)
+redGreen_leds = [ "01", "07", "13","04", "10", "16" ]
+#red: 01, 07, 13
+#green: 04, 10, 16
 
 def random_brightness():
     sleep(random.uniform(0,sleep_period))
-    return random.randint(0,255)
+    return random.randint(100,255)
 
 while True:
     # Switch one random roy LED to one random brightness
-    led_to_switch = int(random.choice(roy_leds))
+    led_to_switch = int(random.choice(redGreen_leds))
     piglow.led(led_to_switch, random_brightness())
-    # Switch one random roy LED off 
-    led_to_switch = int(random.choice(roy_leds))
+    # Switch one random redGreen LED off 
+    led_to_switch = int(random.choice(redGreen_leds))
     piglow.led(led_to_switch, 0)
